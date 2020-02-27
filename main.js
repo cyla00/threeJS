238653660caf4255ca2795288d10babda101c70e
cyla00 <cyla00@protonmail.com>
2020-02-27 15:32:53 +0100

    rotation speed

--- main.js
+++ main.js
@@ -14,12 +14,12 @@ function init(){
   cube = new THREE.Mesh( geometry, material );
   scene.add( cube );
 
-  camera.position.z = 5.
+  camera.position.z = 8.
 }
 
 function animate(){
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
+  cube.rotation.x += 0.005;
   cube.rotation.y += 0.01;
   renderer.render(scene, camera);
 }
